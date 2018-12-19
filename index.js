a161e6c8dd1039bb2cd460d64c2d23aa62530a18
Pittman <wpittman@paypal.com>
2018-12-19 10:20:53 -0600

    new attempt

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("server started on port " + PORT);
 });
